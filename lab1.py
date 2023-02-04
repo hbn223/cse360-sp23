34fbbeb1db0212cd832644f5dd8ac48e4905b8a4
hbn223 <89538240+hbn223@users.noreply.github.com>
2023-02-03 23:50:21 -0500

    Lab 1

---
+++ lab1.py
@@ -0,0 +1,30 @@
+def sense(x):
+    return x
+
+def  simulate(Δt, x, u):
+    x += Δt * u
+    return x
+
+def control(t, y):
+    ux = sin(t)*4
+    uy = cos(t)*2
+    return array([ux, uy])
+
+tf = 7
+Δt = 0.1
+time = linspace(0., tf, int(tf / Δt) + 1 )
+
+#Initial conditions
+x = array([-1., 1.9])
+x_log = [copy(x)]
+
+for t in time:
+    y = sense(x)
+    u = control(t, y)
+    x = simulate(Δt, x, u)
+    x_log.append(copy(x))
+    
+x_log = array(x_log)
+
+grid()
+plot(x_log[:,0], x_log[:,1])
